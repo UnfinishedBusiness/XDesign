0d74ab263caa8b56906abeff2d7950f7a511f35e
Travis Gillin <Travis.gillin@gmail.com>
2019-07-23 21:31:42 -0400

    Zoom to mouse position now works

--- src/JetCad3D/app.js
+++ src/JetCad3D/app.js
@@ -14,25 +14,25 @@ function ParseDXF(data)
 {
   var parser = new DxfParser();
   try {
+      var imported_stack = [];
       var dxf = parser.parseSync(data);
       for (var i = 0; i < dxf.entities.length; i++)
       {
         if (dxf.entities[i].type == "CIRCLE")
         {
-          //mported_stack.push({ type: "circle", origin: [dxf.entities[i].center.x, dxf.entities[i].center.y], radius: dxf.entities[i].radius });
-          DrawArc({ center: { x: dxf.entities[i].center.x, y: dxf.entities[i].center.y }, radius: dxf.entities[i].radius, startAngle: 0, endAngle: 0, direction: true });
+          imported_stack.push({ type: "circle", origin: [dxf.entities[i].center.x, dxf.entities[i].center.y], radius: dxf.entities[i].radius, color: render._colorWhite });
         }
         else if (dxf.entities[i].type == "LINE")
         {
-          //imported_stack.push({ type: "line", origin: [dxf.entities[i].vertices[0].x, dxf.entities[i].vertices[0].y], end: [dxf.entities[i].vertices[1].x, dxf.entities[i].vertices[1].y] });
-          DrawLine({ start: {x: dxf.entities[i].vertices[0].x, y: dxf.entities[i].vertices[0].y, z: dxf.entities[i].vertices[0].z}, end: { x: dxf.entities[i].vertices[1].x, y: dxf.entities[i].vertices[1].y, z: dxf.entities[i].vertices[1].z}});
+          imported_stack.push({ type: "line", origin: [dxf.entities[i].vertices[0].x, dxf.entities[i].vertices[0].y], end: [dxf.entities[i].vertices[1].x, dxf.entities[i].vertices[1].y], color: render._colorWhite });
         }
         else if (dxf.entities[i].type == "ARC")
         {
-          //imported_stack.push({ type: "arc", origin: [dxf.entities[i].center.x, dxf.entities[i].center.y], startAngle: this.to_degrees(dxf.entities[i].startAngle), endAngle: this.to_degrees(dxf.entities[i].endAngle), radius: dxf.entities[i].radius });
-          DrawArc({ center: { x: dxf.entities[i].center.x, y: dxf.entities[i].center.y }, radius: dxf.entities[i].radius, startAngle: dxf.entities[i].startAngle * 180 / Math.PI, endAngle: dxf.entities[i].endAngle * 180 / Math.PI, direction: false });
+          imported_stack.push({ type: "arc", origin: [dxf.entities[i].center.x, dxf.entities[i].center.y], startAngle: render.to_degrees(dxf.entities[i].startAngle), endAngle: render.to_degrees(dxf.entities[i].endAngle), radius: dxf.entities[i].radius, color: render._colorWhite });
         }
       }
+      var part = { part_name: "DXFPart", offset: [0, 0, 0], entities: imported_stack, meta_data: {}, updateRender: true};
+      render.Stack.push(part);
   }catch(err) {
       return console.error(err.stack);
   }
